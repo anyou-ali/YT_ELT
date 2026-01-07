7997148c03d94958584f7c5a93595be6d2ef7609
any-06 <anyou.6.ali@gmail.com>
2026-01-07 15:04:04 +0100

    Added dockerfile and requirements.txt

--- video_stats.py
+++ video_stats.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+CHANNEL_HANDLE = os.getenv("MrBeast")
 maxResults = 50
 
 
